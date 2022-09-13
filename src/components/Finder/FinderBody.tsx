bfc8b2a7a726a4fc588faac3a91be2c1bed198b3
Gautam Anand <gautamanand022@gmail.com>
2022-09-13 15:57:16 +0530

    style: fixes

--- src/components/Finder/FinderBody.tsx
+++ src/components/Finder/FinderBody.tsx
@@ -2,16 +2,22 @@ import { IAssetImages } from "../../interfaces/IAssetImages";
 
 function FinderBody({ imageList }: { imageList: Array<IAssetImages> }) {
   return (
-    <div className="grid grid-rows-3 grid-cols-3 gap-4 px-4">
+    <div className="grid grid-rows-3 grid-cols-3 gap-4 px-4 cursor-default">
       {imageList.map((data) => (
-        <div className="flex flex-col h-full justify-center gap-2 py-2">
+        <div className="flex flex-col h-full justify-center items-center gap-2 py-2 ">
           {data?.type === "mp4" ? (
-            <video className="rounded" src={data.source} autoPlay loop muted />
+            <video
+              className="rounded cursor-pointer"
+              src={data.source}
+              autoPlay
+              loop
+              muted
+            />
           ) : (
             <img
               src={data.source}
               alt=""
-              className="h-36 w-60 rounded-md object-contain"
+              className="h-36 w-60 rounded-md object-contain cursor-pointer border border-gray-300"
             />
           )}
           <span className="text-center text-gray-700">{data.name}</span>
