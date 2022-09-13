9be3edb9ba179cf9bb43166bb4067d12d4bc5dfa
Gautam Anand <gautamanand022@gmail.com>
2022-09-14 00:56:58 +0530

    dropdown

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -10,12 +10,19 @@ import {
 import { MdOutlineWifiTetheringOff } from "react-icons/md";
 import { IoMdDesktop } from "react-icons/io";
 import Moment from "react-moment";
+import { useState } from "react";
+import { systemDropDownoptions } from "../data/systemDropDownList";
 
 function StatusBar() {
   const options = ["Finder", "File", "Edit", "View", "Go", "Window", "Help"];
+  const [isSystemDropdown, setisSystemDropdown] = useState(false);
+
   return (
-    <div className="z-10 h-6 px-2 bg-black/20 backdrop-blur-md w-full overflow-hidden flex gap-2 text-sm justify-between text-white items-center">
-      <BsApple />
+    <div className="z-10 h-6 px-2 bg-white/20 backdrop-blur-md w-full  flex gap-2 text-sm justify-between text-white items-center">
+      <BsApple
+        onClick={() => setisSystemDropdown(!isSystemDropdown)}
+        className="hover:text-gray-700 duration-500 cursor-pointer"
+      />
       {options.map((option, index) => {
         return (
           <button className="hover:text-gray-300 duration-200" key={index}>
@@ -25,23 +32,37 @@ function StatusBar() {
       })}
 
       <div className="ml-auto flex gap-2 items-center">
-        <MdOutlineWifiTetheringOff className="" />
-        <IoMdDesktop className="" />
-        <IoIosCalendar className="" />
-        <FaUserCircle className="" />
-        <IoIosBatteryFull className="" />
-        <IoIosSearch className="" />
-        <IoIosWifi className="" />
-        <IoIosSwitch className="" />
-        <img
-          src="https://static.wikia.nocookie.net/logopedia/images/3/3b/Siri1.png/revision/latest?cb=20160616160932"
-          alt="Siri Logo"
-          className="w-4 h-4"
-        />
+        <MdOutlineWifiTetheringOff />
+        <IoMdDesktop />
+        <IoIosCalendar />
+        <FaUserCircle />
+        <IoIosBatteryFull />
+        <IoIosSearch />
+        <IoIosWifi />
+        <IoIosSwitch />
         <div className="mx-3">
           <Moment format="ddd hh:mm"></Moment>
         </div>
       </div>
+      {isSystemDropdown && (
+        <div className="absolute border px-2 py-2 border-gray-200 top-7 rounded left-1 text-gray-700 text-sm text-left bg-white/80 backdrop-blur-md flex flex-col justify-around gap-2">
+          {systemDropDownoptions.map((option, index) => (
+            <div key={option.id}>
+              <button
+                onClick={option.action}
+                className="text-sm w-full text-left hover:bg-white/70  rounded px-3 duration-300"
+              >
+                {option.name}
+              </button>
+              {(index === 0 ||
+                index === 2 ||
+                index === 3 ||
+                index === 4 ||
+                index === 7) && <hr className="border-slate-400/75 my-1" />}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
