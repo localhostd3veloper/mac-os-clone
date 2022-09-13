62315e3846d3426fdef4a3c30a245db0c302d4d8
Gautam Anand <gautamanand022@gmail.com>
2022-09-14 02:00:53 +0530

    add: ShutDown Screen

---
+++ src/views/ShutDown.tsx
@@ -0,0 +1,10 @@
+import { GrApple } from "react-icons/gr";
+function ShutDown() {
+  return (
+    <div className="flex flex-col text-white h-screen justify-center items-center bg-black">
+      <GrApple className="text-9xl text-white animate-pulse" />
+    </div>
+  );
+}
+
+export default ShutDown;
