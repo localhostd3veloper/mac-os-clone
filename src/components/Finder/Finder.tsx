7e35be56afc6753d4edfa51d2d304a195ab106f1
Gautam Anand <gautamanand022@gmail.com>
2022-09-13 13:49:18 +0530

    feature: Add Full Screen

--- src/components/Finder/Finder.tsx
+++ src/components/Finder/Finder.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, Dispatch } from "react";
+import { SetStateAction, Dispatch, useState } from "react";
 import FinderBody from "./FinderBody";
 import FinderHeader from "./FinderHeader";
 import FinderSidebar from "./FinderSidebar";
@@ -9,17 +9,26 @@ function Finder({
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
   return (
     <>
       {isOpen && (
-        <div className="h-[500px] w-[50vw] cursor-grab  flex bg-white/40 rounded-xl overflow-hidden backdrop-blur-sm">
+        <div
+          className={`${
+            isFullScreen ? "h-[88vh] w-screen " : "h-[500px] w-[50vw]"
+          }  cursor-grab  flex bg-white/40 rounded-xl overflow-hidden backdrop-blur-sm transition-all duration-300`}
+        >
           <div className="w-3/12 flex flex-col gap-4 p-2 cursor-default">
             <div className="flex gap-1 p-2">
-              <span
+              <button
                 className="h-3 w-3 rounded-full bg-red-600 cursor-pointer"
                 onClick={() => setIsOpen(false)}
-              ></span>
-              <span className="h-3 w-3 rounded-full bg-yellow-600"></span>
+              ></button>
+              <button
+                className="h-3 w-3 rounded-full bg-yellow-600"
+                onClick={() => setIsFullScreen(!isFullScreen)}
+              ></button>
               <span className="h-3 w-3 rounded-full bg-green-600"></span>
             </div>
             <FinderSidebar />
--- src/components/Finder/FinderBody.tsx
+++ src/components/Finder/FinderBody.tsx
@@ -2,15 +2,19 @@ import { imageList } from "../../data/imageList";
 
 function FinderBody() {
   return (
-    <div className="grid grid-rows-3 grid-cols-3 gap-4 p-5">
+    <div className="grid grid-rows-3 grid-cols-3 gap-4 px-4">
       {imageList.map((data) => (
-        <div className="flex flex-col h-full justify-center gap-2">
+        <div className="flex flex-col h-full justify-center gap-2 py-2">
           {data?.type === "mp4" ? (
-            <video className="rounded" src={data?.source} autoPlay loop muted />
+            <video className="rounded" src={data.source} autoPlay loop muted />
           ) : (
-            <img src={data.source} alt="" className="h-36 w-60 rounded-md" />
+            <img
+              src={data.source}
+              alt=""
+              className="h-36 w-60 rounded-md object-contain"
+            />
           )}
-          <span className="text-center">{data.name}</span>
+          <span className="text-center text-gray-700">{data.name}</span>
         </div>
       ))}
     </div>
